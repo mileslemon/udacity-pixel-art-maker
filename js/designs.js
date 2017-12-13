af85f0ee60fe49c99aea257d051b0a91b1bdb068
mileslemon <32392238+mileslemon@users.noreply.github.com>
2017-12-13 00:54:59 +0000

    reworked painting, can now click and drag

--- js/designs.js
+++ js/designs.js
@@ -1,5 +1,4 @@
 // document is ready
-// put all code in here
 $(function(){
 
     // creates a grid using sizes inputted by the user.
@@ -9,6 +8,7 @@ $(function(){
         var gridHeight = $('#input_height').val();
         var gridWidth = $('#input_width').val();
 
+        // clears any existing pixel canvas
         pixelCanvas.empty();
 
         for (var i = 0; i < gridHeight; i++) {
@@ -20,20 +20,38 @@ $(function(){
     }
 
     function paintCanvas() {
+        // paints table cells when mouse is down and moused over
+        // changes cell color to background color on shift hover
 
-        // paints table cell with selected color on click
-        // changes cell color to background color on shift click
-        $('.canvasCol').click( function(event) {
-            var color = $('#colorPicker').val();
-            $(this).css("background-color", color);
-            if (event.ctrlKey) {
-                $(this).css("background-color", "#fff");
+        // assumes mouse isnt clicked
+        var clicked = false;
+        var shiftClicked = false;
+
+        // checks if mouse is clicked
+        $('.canvasCol').mousedown(function (event) {
+            clicked = true;
+            // prevents the user from dragging the table
+            event.preventDefault();
+            // checks if shift is held while clicking
+            if (event.shiftKey) {
+                clicked = false;
+                shiftClicked = true;
             }
-        });   
-
+        }).mouseup(function () {
+            clicked = false;
+            shiftClicked = false;
+        }).mousemove(function () {
+            // if mouse is clicked paints the cell the mouse is over
+            if(clicked){
+                var color = $('#colorPicker').val();
+                $(this).css('background-color', color);
+            // if shiftclicked paints the cell to the default color
+            } else if (shiftClicked) {
+                $(this).css('background-color', "#fff"); 
+            }
+        });
     }
     
-
     // calls the makeGrid function on form submission
     // prevents the page from refreshing when submitted
     $('#submitGrid').click( function( event ){
@@ -42,6 +60,4 @@ $(function(){
         paintCanvas();
     });
 
-
 });
-
